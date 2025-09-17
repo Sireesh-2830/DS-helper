9b72b3f9e99180fc59be3aab750f6e8c71628204
unknown <sireeshs812@gmail.com>
2025-09-17 10:11:20 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DS-helper
+# DS-helper
---
+++ first.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+# titanic dataset
+df = pd.read_csv(r"File path of dataset")
+
+def detect_column_types(df, categorical_threshold=20):
+    column_types = {}
+    for col in df.columns:
+        num_unique = df[col].nunique(dropna=False)
+        dtype = df[col].dtype
+
+        if dtype == 'object' or dtype == 'string':
+            column_types[col] = 'text'
+        elif pd.api.types.is_numeric_dtype(dtype):
+            if num_unique < categorical_threshold:
+                column_types[col] = 'categorical'
+            else:
+                column_types[col] = 'numerical'
+        else:
+            column_types[col] = 'categorical'
+    return column_types
+
+
+result = detect_column_types(df)
+print(result)
